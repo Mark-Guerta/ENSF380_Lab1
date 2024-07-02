dfbca8b3d91548e9b96a680af34ebad5e0a82220
Arion <144377385+Mark-Guerta@users.noreply.github.com>
2024-07-02 16:40:30 -0600

    Added java project

--- src/com/markguerta/calculator/Application.java
+++ src/com/markguerta/calculator/Application.java
@@ -1,5 +1,16 @@
 package com.markguerta.calculator;
-
+/**
+* <h1 > Application.java </h1 >
+* <p>
+* This class is designed for displaying "Hello, World!" in Java.
+* It is part of Lab 1 Exercise A 2.6.
+* </p>
+*
+* <p><b> Submission Date :</b> June 2, 2024 </p>
+*
+* @author Mark Guerta
+* @version 1.0
+*/
 public class Application {
 
 }
