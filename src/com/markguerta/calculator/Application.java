636c82a0f6f8aee9d573beb1524dc2934271dc47
Arion <144377385+Mark-Guerta@users.noreply.github.com>
2024-07-02 17:21:17 -0600

    Completed basic calculator
    Testing and comments needed

--- src/com/markguerta/calculator/Application.java
+++ src/com/markguerta/calculator/Application.java
@@ -1,4 +1,3 @@
-package com.markguerta.calculator;
 /**
 * <h1 > Application.java </h1 >
 * <p>
@@ -11,6 +10,64 @@ package com.markguerta.calculator;
 * @author Mark Guerta
 * @version 1.0
 */
+package com.markguerta.calculator;
+import java.util.Scanner;
 public class Application {
-
+	public static void main(String[] args) {
+		if (args.length > 0) {
+			
+		}
+		else {
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Enter operation (e.g. add, sub, mul, div, factorial):");
+			String operation = scanner.next();
+			double x = Double.parseDouble(args[0]);
+			double y = 0;
+			if (args.length == 1)
+				 y = Double.parseDouble(args[1]);
+			if (operation == "add")
+				System.out.println("The answer is " + add(x, y));
+			else if (operation == "sub")
+				System.out.println("The answer is " + sub(x, y));
+			else if (operation == "mul")
+				System.out.println("The answer is " + mul(x, y));
+			else if (operation == "div")
+				if (div(x,y) == 0)
+					System.out.println("The answer is undefined");
+				else
+					System.out.println("The answer is " + div(x, y));
+			else if (operation == "factorial")
+				System.out.println("The answer is " + factorial(x));
+			scanner.close();
+		}
+		
+	}
+	
+	public static double add(double a, double b) {
+		return a + b;
+	}
+	
+	public static double sub(double c, double d) {
+		return c - d;
+	}
+	
+	public static double mul(double e, double f) {
+		return e * f;
+	}
+	
+	public static double div(double g, double h) {
+		if (h == 0)
+			return 0;
+		else
+			return g/h;
+	}
+	
+	public static double factorial(double n) {
+		if (n <= 1)
+			return 1;
+		else {
+			return n * factorial(n - 1);
+		}
+	}
+	
 }
